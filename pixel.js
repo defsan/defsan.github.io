889475b8cd7b8cd8e50dd1202ad6dc4ea58fc234
defsan <elie@ifttt.com>
2021-06-23 11:19:28 -0700

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -1,3 +1,45 @@
+function _triggerEvents() {
+  var value = Math.floor(Math.random() * Math.floor(100));
+  var params = {
+    value: value,
+    currency: 'USD',
+    contents: [
+      {
+        id: '101',
+        quantity: 1
+      },
+      {
+        id: '102',
+        quantity: 2
+      }],
+    compared_product: 'recommended-banner-shoes',  // custom property
+    content_type: 'product',
+    delivery_category: 'in_store'
+  };
+  fbq('track', 'Purchase', params);
+  fbq('track', 'AddToCart', params);
+  fbq('track', 'ViewContent', params, {eventID: new Date()});
+  fbq('trackCustom', 'ACustomEvent', {custom_param: 'custom_value'});
+  document.getElementById("main").innerHTML = "Triggered";
+}
+
+
+function purchase(price) {
+    let params = {
+      value: price,
+      currency: 'USD',
+      contents: [
+        {
+          id: '101',
+          quantity: 1
+        }],
+      compared_product: 'recommended-banner-shoes',  // custom property
+      content_type: 'product',
+      delivery_category: 'in_store'
+    };
+  fbq('track', 'Purchase', params);
+}
+
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -8,3 +50,5 @@ s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '520509209187583');
 fbq('track', 'PageView');
+
+
